251e9e07f6552fa431cb619b0cc812b84366fa53
Artha <arthapotdar@gmail.com>
2021-07-22 16:57:48 +0530

    ngIf part 4 vid 2

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("ngOnInit works !");
+    // setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Artha',
+          lastName: 'Potdar',
+          age: 19,
+          address: {
+              flat: 403,
+              building: 'Simran Sunshine',
+              area: 'Nipaniya',
+              city: 'Indore',
+              state: 'MP'
+          }
+        },
+        {
+          firstName: 'Anushka',
+          lastName: 'Nagar',
+          age: 20,
+          address: {
+              flat: 30,
+              building: 'Grasim Staff Colony',
+              area: 'Birlagram',
+              city: 'Nagda',
+              state: 'MP'
+          }
+        },
+        {
+          firstName: 'Giraj',
+          lastName: 'Neema',
+          age: 21,
+          address: {
+              flat: 101,
+              building: 'Moonlight View',
+              area: 'Malganj',
+              city: 'Indore',
+              state: 'MP'
+          }
+        }
+      ];
+
+      this.loaded = true;
+    
+    // 2000);
+
+    // this.showExtended = false;
+
+    this.addUser({
+      firstName: 'Deepanshi',
+      lastName: 'Joshi'
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,12 +1,12 @@
 export interface User {
     firstName: string,
     lastName: string,
-    age: number,
-    address: {
-        flat: number,
-        building: string,
-        area: string,
-        city: string,
-        state: string
+    age?: number,
+    address?: {
+        flat?: number,
+        building?: string,
+        area?: string,
+        city?: string,
+        state?: string
     }
 }
